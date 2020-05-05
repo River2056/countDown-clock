41650729bcca75f256eca3dd19922e6612141cdd
Kevin.Tung <chen0625tung@gmail.com>
2020-05-05 18:26:53 +0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function getNewTime() {
+    const date = new Date();
+    const destination = new Date(2021, 6, 1);
+    console.log(destination);
+    let timeText = document.getElementById('time-text');
+    const difference = destination.getTime() - date.getTime();
+    
+    let totalTime = difference;
+    const days = Math.floor(totalTime / 1000 / 60 / 60 / 24);
+    totalTime -= days * 1000 * 60 * 60 * 24;
+
+    const hours = Math.floor(totalTime / 1000 / 60 / 60);
+    totalTime -= hours * 1000 * 60 * 60;
+
+    const mins = Math.floor(totalTime / 1000 / 60);
+    totalTime -= mins * 1000 * 60;
+
+    const secs = Math.floor(totalTime / 1000);
+
+    console.log(`${days}day ${hours}hours ${mins}mins ${secs}secs`);
+    timeText.innerHTML = `距離2020年7月1日 還有${days}天${hours}時${mins}分${secs}秒`
+}
+
+setInterval(getNewTime, 500);
